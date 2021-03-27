194a512361a8dcc3e513f7dfd3145957e6a779de
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-03-27 15:51:53 +0000

    prod first try

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routes import items
-from config import PROJECT_NAME
+from config import PROJECT_NAME, BACKEND_URL_PREFIX
 
-app = FastAPI(title=f'{PROJECT_NAME} API')
+app = FastAPI(title=f'{PROJECT_NAME} API', root_path=BACKEND_URL_PREFIX)
 app.include_router(items.router)
 
 
 @app.get('/', tags=['root'])
-async def index():
-    return {'status': 'online'}
+async def index(request: Request):
+    return {'status': 'online', 'version': '1'}
