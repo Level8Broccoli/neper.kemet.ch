685490d1d57b3f1cdd8f058ce259dba4bbee87b0
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-02 14:05:20 +0000

    restart with next for admin page

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,36 +1,9 @@
-import Link from 'next/link';
+export default function AdminHome() {
+  const re = 'ho';
 
-export default function Home({ data }) {
   return (
     <main>
-      <h1>Alle Items</h1>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
-            <Link href={'/' + String(item.id)}>
-              <a>Zur Item-Seite</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1>Admin2</h1>
     </main>
   );
-}
-
-export async function getStaticProps(context) {
-  const host = process.env.API_HOST || 'http://localhost:8000';
-  const res = await fetch(`${host}/items`);
-  const data = await res.json();
-
-  if (!data) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: { data },
-  };
 }
